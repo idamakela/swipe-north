089877c5e13a2b5d050f6d9171a44854f7aae3ed
idamakela <idamakela98@gmail.com>
2023-03-20 13:41:10 +0100

    configed sass

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    experimental: {
+        appDir: true,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+};
 
-module.exports = {
-  nextConfig,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')], 
-  }
-}
+module.exports = nextConfig;
