852a2be0e9bf61bea5d352fa4b64ac2b9954f5b3
Ida Mäkelä <idamakela98@gmail.com>
2023-03-20 19:54:15 +0100

    stuck on slider

--- app/swiper/page.js
+++ app/swiper/page.js
@@ -1,3 +1,23 @@
+import { register } from "swiper/element/bundle";
+import styles from "../../styles/page.module.scss";
+
 export default function Swiper() {
-    return <h1>HI. this is the swiper page</h1>;
+    register();
+
+    return (
+        <>
+            <h2 className='swiper-title'>HI. this is the swiper page</h2>
+            <div className={styles.cards}>
+                <swiper-container
+                    className={styles.container}
+                    slides-per-view='1'
+                    loop='true'
+                >
+                    <swiper-slide className={styles.card}>Slide 1</swiper-slide>
+                    <swiper-slide className={styles.card}>Slide 2</swiper-slide>
+                    <swiper-slide className={styles.card}>Slide 3</swiper-slide>
+                </swiper-container>
+            </div>
+        </>
+    );
 }
