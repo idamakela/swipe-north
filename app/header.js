8bf23be2b533c6a3483c1eba35438bd5d3be07a4
lutxi <aliaga_sheila@hotmail.com>
2023-03-21 21:24:51 +0100

    add jobs data object

---
+++ app/header.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+//import styles from
+
+export default function header(){
+    return(
+        <>
+        <header>
+            <div className="logo">
+                
+            </div>
+            <div className="hjärta">
+
+            </div>
+        </header>
+        </>
+    )
+}
--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header />
+        {children}
+      </body>
     </html>
   )
 }
