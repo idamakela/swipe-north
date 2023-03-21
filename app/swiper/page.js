91826303512280689df427c926938cb1c73d7fdd
Ida Mäkelä <idamakela98@gmail.com>
2023-03-21 11:24:25 +0100

    started using tindercard with .map

--- app/swiper/page.js
+++ app/swiper/page.js
@@ -2,25 +2,38 @@
 import { useState } from "react";
 import TinderCard from "react-tinder-card";
 import Cards from "./cards";
-import styles from "../../styles/swiper.modules.css";
+import styles from "../../styles/swiper.module.scss";
 
 export default function Swiper() {
     const [job, setJob] = useState([]);
 
     return (
         <>
-            <div>
-                <h2 className={styles.title}>
-                    HI. this is the swiper page select
-                </h2>
+            <div className={styles.title}>
+                <h2>HI. this is the swiper page select</h2>
             </div>
             <div className={styles.cards}>
                 {job.map((job) => (
                     <TinderCard>
                         <div className={styles.card}>
-                            <div className={styles.titles}>
+                            <div>
+                                <img
+                                    src={job.img}
+                                    alt={job.imgAlt}
+                                    height='500px'
+                                    width='500px'
+                                />
+                            </div>
+                            <div className={styles.text}>
                                 <h3>{job.title}</h3>
                                 <h4>{job.company}</h4>
+                                <p>{`${job.icon} ${job.type}%`}</p>
+                                <p>{job.desc}</p>
+                            </div>
+                            <div className={styles.btnContainer}>
+                                <button onClick={funciton}>back</button>
+                                <button onClick={funciton}>heart</button>
+                                <button onClick={funciton}>remove</button>
                             </div>
                         </div>
                     </TinderCard>
