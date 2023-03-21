74714f0aea4d9f3696928ba1485023df51042fb3
Ida Mäkelä <idamakela98@gmail.com>
2023-03-21 14:11:49 +0100

    base layout done, now more styling

--- app/swiper/cards.js
+++ app/swiper/cards.js
@@ -1,9 +1,34 @@
 import TinderCard from "react-tinder-card";
+import styles from "../../styles/swiper.module.scss";
 
-export default function Cards({ ...job }) {
+export default function Cards({ id, img, imgAlt, title, company, icon, type, desc }) {
     return (
-        <TinderCard>
-            <div>{}</div>
+        <TinderCard
+            className={styles.swipe}
+            key={id}
+            preventSwipe={["down"]}
+        >
+            <div className={styles.card}>
+                <div>
+                    <img
+                        src={img}
+                        alt={imgAlt}
+                        height='200px'
+                        width='200px'
+                    />
+                </div>
+                <div className={styles.text}>
+                    <h3>{title}</h3>
+                    <h4>{company}</h4>
+                    <p>{`${icon} ${type}%`}</p>
+                    <p>{desc}</p>
+                </div>
+                <div className={styles.btnContainer}>
+                    <button>back</button>
+                    <button>heart</button>
+                    <button>remove</button>
+                </div>
+            </div>
         </TinderCard>
     );
 }
--- app/swiper/page.js
+++ app/swiper/page.js
@@ -1,42 +1,42 @@
 "use client";
 import { useState } from "react";
-import TinderCard from "react-tinder-card";
 import Cards from "./cards";
 import styles from "../../styles/swiper.module.scss";
 
 export default function Swiper() {
-    const [job, setJob] = useState([]);
+    const [job, setJob] = useState([
+        {
+            id: 1,
+            img: "",
+            alt: "img alt",
+            title: "caffis",
+            company: "byrå",
+            icon: "home",
+            type: 100,
+            desc: "hdjks fsdkjöahf djsiaöjfeia öjsdöjfieö ajfdköasf jei sjdklöf jeisa ö",
+        },
+        {
+            id: 2,
+            img: "",
+            alt: "img alt",
+            title: "chef",
+            company: "max",
+            icon: "home",
+            type: 100,
+            desc: "hdjks fsdkjöahf djsiaöjfeia öjsdöjfieö ajfdköasf jei sjdklöf jeisa ö",
+        },
+    ]);
+
+    //setJob[dataFile];
 
     return (
         <>
             <div className={styles.title}>
-                <h2>HI. this is the swiper page select</h2>
+                <h2>this is filter "menu"</h2>
             </div>
-            <div className={styles.cards}>
+            <div className={styles.cardsContainer}>
                 {job.map((job) => (
-                    <TinderCard>
-                        <div className={styles.card}>
-                            <div>
-                                <img
-                                    src={job.img}
-                                    alt={job.imgAlt}
-                                    height='500px'
-                                    width='500px'
-                                />
-                            </div>
-                            <div className={styles.text}>
-                                <h3>{job.title}</h3>
-                                <h4>{job.company}</h4>
-                                <p>{`${job.icon} ${job.type}%`}</p>
-                                <p>{job.desc}</p>
-                            </div>
-                            <div className={styles.btnContainer}>
-                                <button onClick={funciton}>back</button>
-                                <button onClick={funciton}>heart</button>
-                                <button onClick={funciton}>remove</button>
-                            </div>
-                        </div>
-                    </TinderCard>
+                    <Cards key={job.id} {...job} />
                 ))}
             </div>
         </>
