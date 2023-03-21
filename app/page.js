a9a9037da6cd9039165e1b9309352a088ecbb4c5
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-03-21 15:25:15 +0100

    add landingpage & css variables

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Readex_Pro } from "next/font/google";
 import styles from "../styles/page.module.scss";
+import { FaAngleUp } from 'react-icons/fa'
 
-const inter = Inter({ subsets: ["latin"] });
+  const readex_pro = Readex_Pro({
+    weight: '400',
+    subsets: ['latin'],
+  })
+
 
 export default function Home() {
-    return <main>HI. this is the landing page</main>;
+    return <main className={`${readex_pro.className} ${styles.main}`}>
+             <div className={styles.hero}>
+              <h1 className={styles.title} >Swipe north</h1>
+              <div className={styles.animationContainer}>
+                <div className={styles.angleAnimation}>
+                  <p className={styles.small}><FaAngleUp/></p>
+                  <p className={styles.medium}><FaAngleUp/></p>
+                  <p className={styles.big}><FaAngleUp/></p>
+                 </div>
+                <h2 className={styles.swipe}>SWIPE</h2>
+              </div>
+             </div>
+        </main>;
 }
