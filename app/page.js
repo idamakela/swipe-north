24eb29ccb7e85c98f25be82a69a2ed8757c5b931
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-03-22 12:03:28 +0100

    change backround & link to swiper

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-
 import { Readex_Pro } from "next/font/google";
 import styles from "../styles/page.module.scss";
 import { FaAngleUp } from 'react-icons/fa'
+import Link from 'next/link';
 
   const readex_pro = Readex_Pro({
     weight: '400',
@@ -15,11 +15,13 @@ export default function Home() {
               <h1 className={styles.title} >Swipe north</h1>
               <div className={styles.animationContainer}>
                 <div className={styles.angleAnimation}>
+                 <Link href="/swiper" >
                   <p className={styles.small}><FaAngleUp/></p>
                   <p className={styles.medium}><FaAngleUp/></p>
                   <p className={styles.big}><FaAngleUp/></p>
+                  </Link>
                  </div>
-                <h2 className={styles.swipe}>SWIPE</h2>
+                <Link href="/swiper" ><h2 className={styles.swipe}>SWIPE</h2></Link>
               </div>
              </div>
         </main>;
