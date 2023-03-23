c3b4373b6cd9b61b993b7e855cb967eb3186b12b
Ida Mäkelä <idamakela98@gmail.com>
2023-03-23 12:55:27 +0100

    code for only loading 2 cards at a time

--- app/swiper/page.jsx
+++ app/swiper/page.jsx
@@ -5,7 +5,13 @@ import Cards from './cards.jsx';
 import styles from '../../styles/swiper.module.scss';
 
 export default function Swiper() {
-    const [job, setJob] = useState(data);
+    const [jobs, setJobs] = useState(data.slice(0, 2));
+
+    const loadMoreJobs = () => {
+        const currentLength = jobs.length;
+        const nextJobs = data.slice(currentLength, currentLength + 2);
+        setJobs([...jobs, ...nextJobs]);
+    }
 
     return (
         <>
@@ -13,8 +19,9 @@ export default function Swiper() {
                 <h2>this is filter "menu"</h2>
             </div>
             <div className={styles.cardsContainer}>
-                {job.map((job) => (
+                {jobs.map((job) => (
                     <Cards key={job.id} {...job} />
+                    /* on swipe: loadMoreJobs */
                 ))}
             </div>
         </>
