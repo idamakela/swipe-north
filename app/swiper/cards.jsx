9c113f9714f2ed3621e6eebcce1a0fc21365378c
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-03-28 13:39:06 +0200

    add styles bigcard

--- app/swiper/cards.jsx
+++ app/swiper/cards.jsx
@@ -17,6 +17,7 @@ import Icon from '../icon.js';
 // continue functions from this example: https://github.com/3DJakob/react-tinder-card-demo/blob/master/src/examples/Advanced.js
 
 export default function Cards({
+    id,
     picture,
     /*imgAlt,*/
     title,
@@ -63,39 +64,43 @@ export default function Cards({
 
     return (
         <div className={styles.card}>
-            <div className={styles.upperElements}>
-                <div className={styles.imgContainer}>
-                    <img
-                        src={picture}
-                        /*alt={imgAlt}*/ height="200px"
-                        width="200px"
-                    />
-                </div>
-                <div className={styles.text}>
-                    <div className={styles.header}>
-                        <h3 className={styles.title}>{title}</h3>
-                        <h4 className={styles.company}>{company}</h4>
-                        <button className={styles.more}>
-                            <RxDoubleArrowUp />
-                        </button>
-                    </div>
-                    <p
-                        className={styles.icons}
-                    >{`${icon} ${percentajeJob}%`}</p>
-                    <p>{`${smallDesc}...`}</p>
-                </div>
+        <div className={styles.upperElements}>
+            <div className={styles.imgContainer}>
+                <img
+                    src={currentJob.picture}
+                    /*alt={imgAlt}*/ height="200px"
+                    width="200px"
+                />
             </div>
-            <div className={styles.btnContainer}>
-                <button className={styles.back}>
-                    <Revert />
-                </button>
-                <button className={styles.heart}>
-                    <FaHeart />
-                </button>
-                <button className={styles.remove}>
-                    <Close />
-                </button>
+            <div className={styles.text}>
+                <div className={styles.header}>
+                    <h3 className={styles.title}>{currentJob.title}</h3>
+                    <h4 className={styles.company}>{currentJob.company}</h4>
+                    <button className={styles.more} onClick={handleClick}>
+                        <RxDoubleArrowUp />
+                    </button>
+                </div>
+                {/* this below needs logic to set right icon */}
+                <p
+                    className={styles.icons}>{`${currentJob.icon} ${currentJob.percentajeJob}%`}</p>
+                <p>{`${currentJob.smallDesc}...`}</p>
             </div>
         </div>
+        <div className={styles.btnContainer}>
+            <button className={styles.back} disabled={currentJobIndex === 0} onClick={handlePrevClick}>
+                <Revert />
+            </button>
+            <button className={styles.heart} onClick={handleSaveClick}>
+                <FaHeart />
+            </button>
+            <button className={styles.remove}  disabled={currentJobIndex === jobs.length - 1} onClick={handleNextClick}>
+                <Close />
+            </button>
+        </div>
+         {showModal && (
+      <BigCard id={id} job={currentJob} setShowModal={setShowModal}/>
+    )}
+    </div>
+   
     );
 }
