cd96da6975ec4b1b64e813c1cbb977379df71a03
idamakela <idamakela98@gmail.com>
2023-03-28 11:14:24 +0200

    swiper icon not working

--- app/swiper/cards.jsx
+++ app/swiper/cards.jsx
@@ -1,4 +1,8 @@
-import { FaHeart } from 'react-icons/fa';
+import { FaHeart, FaTaxi } from 'react-icons/fa';
+import {BsWifi} from 'react-icons/bs';
+import {HiHome} from 'react-icons/hi';
+import { MdOutlineWork } from "react-icons/md";
+import {SiHomeassistant} from 'react-icons/si'
 import { Close, Revert } from 'grommet-icons';
 import { RxDoubleArrowUp } from 'react-icons/rx';
 import styles from '../../styles/swiper.module.scss';
@@ -16,27 +20,34 @@ export default function Cards({
 }) {
 
     //placeholder medans nedan
-    const icon = 'icon';
+    var icon = null;
+    console.log(iconDescription.transport)
 
     function iconVar () {
         //iconDescription[i] if true => correct icon
         switch (true) {
             case iconDescription.transport:
                 //icon for transport
+                icon = <FaTaxi />
                 break;
             case iconDescription.remote:
                 //icon for remote
+                icon = <BsWifi />
                 break;
             case iconDescription.local:
                 //icon for local/office
+                icon = <HiHome />
                 break;
             case iconDescription.hybrid:
                 //icon for hybrid
+                icon = <SiHomeassistant />
                 break;
             default:
+                icon = <MdOutlineWork />
                 //icon for default
         }
     }
+    iconVar()
 
     const fullDesc = jobbDescription;
     const words = fullDesc.split(' ');
