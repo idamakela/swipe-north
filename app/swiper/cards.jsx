c7aa12b1315da40b80f728dbc5d7ed40f0566db9
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-03-29 16:20:38 +0200

    add big modal

--- app/swiper/cards.jsx
+++ app/swiper/cards.jsx
@@ -1,6 +1,8 @@
 'use client';
+import { useState } from 'react';
 import { RxDoubleArrowUp } from 'react-icons/rx';
 import styles from '../../styles/swiper.module.scss';
+import BigCard from './BigCard';
 
 export default function Cards({
     id,
@@ -21,6 +23,11 @@ export default function Cards({
     const words = fullDesc.split(' ');
     const selectedWords = words.slice(0, 15);
     const smallDesc = selectedWords.join(' ');
+    const [showModal, setShowModal] = useState(false)
+
+    const handleClick = () => {
+     setShowModal(true)
+    }
 
     return (
         <div className={styles.card}>
@@ -32,7 +39,7 @@ export default function Cards({
                     <div className={styles.header}>
                         <h3 className={styles.title}>{title}</h3>
                         <h4 className={styles.company}>{company}</h4>
-                        <button className={styles.more} /*modal onclick här*/>
+                        <button className={styles.more} onClick={handleClick}>
                             <RxDoubleArrowUp />
                         </button>
                     </div>
@@ -42,6 +49,9 @@ export default function Cards({
                     <p>{`${smallDesc}...`}</p>
                 </div>
             </div>
+            {showModal && (
+              <BigCard id={id} title={title}  company={company} setShowModal={setShowModal} jobbDescription={jobbDescription} experience={experience} aboutUs={aboutUs}/>
+            )}
         </div>
     );
 }
